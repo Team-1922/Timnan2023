90af8c7285ee0220ff48acb759a1dcd77c7176fc
schindm <schindm@users.noreply.github.com>
2023-01-11 20:30:43 -0500

    We left of on velocity

--- src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -5,25 +5,41 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import com.revrobotics.CANSparkMax;
+import frc.robot.Constants;
+import com.revrobotics.SparkMaxPIDController;
 public class DriveTrainSubsystem extends SubsystemBase { 
-  private CanSparkMax m_leftlead;
-  private CanSparkMax m_followMotor;
-
+  private CANSparkMax m_leftLead = new CANSparkMax(Constants.leftLead);
+  private CANSparkMax m_leftFollow = new CANSparkMax(Constants.leftFollow);
+  private CANSparkMax m_rightLead = new CANSparkMax(Constants.rightLead);
+  private CANSparkMax m_rightFollow = new CANSparkMax(Constants.rightFollow);
+  private SparkMaxPIDController m_pidControllerLeft;
+  private SparkMaxPIDController m_pidControllerRight;
   /** Creates a new DriveTrainSubsystem. */
   public DriveTrainSubsystem() {
     //Motor controlers
-   
-
-
-
-
-
-
+    m_leftLead.restoreFactoryDefaults();
+    m_leftFollow.restoreFactoryDefaults();
+    m_rightLead.restoreFactoryDefaults();
+    m_rightFollow.restoreFactoryDefaults();
+    m_leftFollow.follow(m_leftLead);
+    m_rightFollow.follow(m_rightLead);
+    m_pidControllerLeft = m_leftLead.getPIDController();
+    m_pidControllerRight = m_rightLead.getPIDController();
   }
+public void Drive(double leftSpeed, double rightSpeed){
+  m_leftLead.set(leftSpeed);
+  m_rightLead.set(rightSpeed);
 
+}
+public void velocityDrive(double velocity){
+  m_pidControllerLeft.setRefrence();
+  m_pidControllerRight.setRefrence();
+}
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  
+
   }
 }
