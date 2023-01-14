1808f81806b7127ec96c72322292a6d3fd94c297
Teddy E <122415127+TeddyE-E@users.noreply.github.com>
2023-01-14 15:12:34 -0500

    added some empty lines to arm subsystem

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -8,7 +8,11 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm extends SubsystemBase {
   /** Creates a new ARM. */
-  public Arm() {}
+  public Arm() {
+
+
+    
+  }
 
   @Override
   public void periodic() {
