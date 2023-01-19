2dded26ef2842039f519ea0e16e8d39f99abb091
Logan Grant <grantlogan2005@gmail.com>
2023-01-18 19:46:44 -0500

    PIDCommanded for autobalance

--- src/main/java/frc/robot/commands/AutoBalance.java
+++ src/main/java/frc/robot/commands/AutoBalance.java
@@ -4,35 +4,43 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.PIDCommand;
 import frc.robot.subsystems.DriveTrainSubsystem;
 
-public class AutoBalance extends CommandBase {
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class AutoBalance extends PIDCommand {
   /** Creates a new AutoBalance. */
-  DriveTrainSubsystem m_driveTrain;
+  private DriveTrainSubsystem m_driveTrain; 
+
 
   public AutoBalance(DriveTrainSubsystem driveTrain) {
-    m_driveTrain = driveTrain;
+    super(
+        // The controller that the command will use
+        new PIDController(0, 0, 0), // Need to insert proper pid values here, waiting until testing
+        // This should return the measurement
+        () -> driveTrain.robotYaw(),
+        // This should return the setpoint (can also be a constant)
+        0,
+        // This uses the output
+        output -> {
+          // Use the output here
+          driveTrain.Drive(output, output);
+        });
+
+        m_driveTrain = driveTrain;
     // Use addRequirements() here to declare subsystem dependencies.
-
-    addRequirements(m_driveTrain);
+        addRequirements(m_driveTrain);
+    // Configure additional PID options by calling `getController` here.
+    getController().setTolerance(3); //this is the angular range (deg) that it is okay stopping in, also waiting until testing
+    getController().enableContinuousInput(-180, 180);
   }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return getController().atSetpoint();
   }
 }
