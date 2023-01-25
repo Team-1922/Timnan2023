39f09d7cf1d668783d13e992117dc1b8a408d449
Teddy E <122415127+TeddyE-E@users.noreply.github.com>
2023-01-24 19:30:06 -0500

    fixed maxrpm so it updates using smartdashboard

--- src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -28,13 +28,13 @@ public class DriveTrainSubsystem extends SubsystemBase {
   private SparkMaxPIDController m_pidControllerLeft;
   private SparkMaxPIDController m_pidControllerRight;
   double kp, ki, kd, kff, kiz, kmaxrpm;
- double rightkp, rightki, rightkd, rightkff, rightkiz, rightmaxrpm;
+ double rightkp, rightki, rightkd, rightkff, rightkiz, krightmaxrpm;
  double kMinOutput, kMaxOutput;
 double RightkMinOutput, RightkMaxOutput;
 
 
 
-double p,i,d,ff,iz,Maxrpm,rightp,righti,rightd,rightff,rightiz,rightMaxrpm;
+double p,i,d,ff,iz,Maxrpm,rightp,righti,rightd,rightff,rightiz,rightmaxrpm;
 double minoutput, maxoutput, rightminoutput, rightmaxoutput;
   /** Creates a new DriveTrainSubsystem. */
   public DriveTrainSubsystem() {
@@ -55,7 +55,7 @@ double minoutput, maxoutput, rightminoutput, rightmaxoutput;
     SmartDashboard.putNumber("right d gain", rightkd); 
     SmartDashboard.putNumber("right feed foward ", rightkff);
     SmartDashboard.putNumber("right i zone", rightkiz);
-    SmartDashboard.putNumber("right max rpm", rightmaxrpm);
+    SmartDashboard.putNumber("right max rpm", krightmaxrpm);
     SmartDashboard.putNumber("right max output", RightkMaxOutput);
     SmartDashboard.putNumber("right min output", RightkMinOutput);
 
@@ -70,7 +70,8 @@ double minoutput, maxoutput, rightminoutput, rightmaxoutput;
     m_pidControllerRight = m_rightLead.getPIDController();}
 
 
-  private AHRS m_navX; 
+  private AHRS m_navX;
+  private double krightMinOutput; 
   /** Creates a new DriveTrainSubsystem. */
   public DriveTrainSubsystem(AHRS navX) {
     //Motor controlers
@@ -101,7 +102,7 @@ public void velocityDrive(double velocity){
  m_pidControllerLeft.setFF(ff); //feed foward
 m_pidControllerLeft.setIZone(iz); //i zone
 
-double leftsetpoint = RobotContainer.LeftJoystick.getY()*kmaxrpm;
+double leftsetpoint = RobotContainer.LeftJoystick.getY()*Maxrpm;
 m_pidControllerLeft.setReference(leftsetpoint, CANSparkMax.ControlType.kVelocity);
 
      // right side
@@ -109,10 +110,10 @@ m_pidControllerRight.setP(rightp);
 
 m_pidControllerRight.setI(righti);
 m_pidControllerRight.setD(rightd);
-m_pidControllerRight.setOutputRange(rightminoutput, rightmaxoutput);
+m_pidControllerRight.setOutputRange(rightminoutput, RightkMaxOutput);
 m_pidControllerRight.setFF(rightff);
 m_pidControllerRight.setIZone(rightd);
-double rightsetpoint = RobotContainer.RightJoystick.getY()*kmaxrpm;
+double rightsetpoint = RobotContainer.RightJoystick.getY()*rightmaxrpm;
 m_pidControllerRight.setReference(rightsetpoint, CANSparkMax.ControlType.kVelocity);
 
 
@@ -133,19 +134,19 @@ if (i != ki) {  m_pidControllerLeft.setI(SmartDashboard.getNumber("left i gain",
  if (d != kd){ m_pidControllerLeft.setD(SmartDashboard.getNumber("left d gain", 0));}
   if (ff != kff) {m_pidControllerLeft.setFF(SmartDashboard.getNumber("left feed foward", 0)); } //feed foward
    if (iz != kiz) {m_pidControllerLeft.setIZone(SmartDashboard.getNumber("left i zone", 0));}
-if (minoutput != kMinOutput){minoutput = kMinOutput;}
-if (maxoutput != kMaxOutput){maxoutput = kMaxOutput;}
-
+  if (minoutput != kMinOutput){minoutput = SmartDashboard.getNumber("left min output", 0);}
+  if (maxoutput != kMaxOutput){maxoutput = SmartDashboard.getNumber("left max output", 1);}
+if (Maxrpm != kmaxrpm) {Maxrpm = SmartDashboard.getNumber("left max rpm", 10);}
 
    // right pid
-    if (rightp != rightkp){ m_pidControllerRight.setP(SmartDashboard.getNumber("right p gain", 0));}
+   if (rightp != rightkp){ m_pidControllerRight.setP(SmartDashboard.getNumber("right p gain", 0));}
    if (righti != rightki) m_pidControllerRight.setI(SmartDashboard.getNumber("right i gain", 0));
-  if (rightd != rightkd) { m_pidControllerRight.setD(SmartDashboard.getNumber("right d gain", 0));}
+   if (rightd != rightkd) { m_pidControllerRight.setD(SmartDashboard.getNumber("right d gain", 0));}
    if (rightff != rightkff){ m_pidControllerRight.setFF(SmartDashboard.getNumber("right feed foward", 0));}
    if (rightiz != rightkiz){ m_pidControllerRight.setIZone(SmartDashboard.getNumber("right i zone", 0));}
-   if (rightmaxoutput != kMaxOutput) {rightmaxoutput = kMaxOutput;}
-    
-
+   if (RightkMaxOutput != rightmaxoutput) {rightmaxoutput =SmartDashboard.getNumber("right max output", 1);}
+   if (krightMinOutput != rightminoutput) {krightMinOutput = SmartDashboard.getNumber("right min output", 0);}
+   if (rightmaxrpm != krightmaxrpm) {rightmaxrpm = SmartDashboard.getNumber("right max rpm", 10);}
 
 
 
