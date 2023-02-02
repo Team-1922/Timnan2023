fdd68008412f390d68cfce722bd508344ca2b913
Teddy E <122415127+TeddyE-E@users.noreply.github.com>
2023-02-01 19:12:10 -0500

    updated tankdrive

--- src/main/java/frc/robot/commands/TankDrive.java
+++ src/main/java/frc/robot/commands/TankDrive.java
@@ -7,6 +7,7 @@ package frc.robot.commands;
 import java.util.concurrent.atomic.DoubleAccumulator;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.DriveTrainSubsystem;
@@ -17,14 +18,15 @@ public class TankDrive extends CommandBase {
    Joystick LeftJoystick;
    Joystick RightJoystick;
   /** Creates a new TankDrive. */
-  public TankDrive(DriveTrainSubsystem driveTrain, Joystick m_LeftJoystick, Joystick m_RightJoystick
+  public TankDrive(DriveTrainSubsystem m_driveTrain, Joystick m_LeftJoystick, Joystick m_RightJoystick
 
   ) {
-  m_DriveTrainSubsystem = driveTrain;
+  m_DriveTrainSubsystem = m_driveTrain;
      LeftJoystick = m_LeftJoystick;
      RightJoystick = m_RightJoystick;
    
-  
+  SmartDashboard.putNumber("leftjoystick", LeftJoystick.getY());
+  SmartDashboard.putNumber("Rightjoystick", RightJoystick.getRawAxis(2));
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(m_DriveTrainSubsystem);
   }
@@ -43,9 +45,11 @@ public class TankDrive extends CommandBase {
   @Override
   public void execute() {
     
+  SmartDashboard.putNumber("leftjoystick", LeftJoystick.getY());
+  SmartDashboard.putNumber("Rightjoystick", RightJoystick.getRawAxis(2));
 
     //Something isnt connecting here -- look into why no get input
-   m_DriveTrainSubsystem.Drive( LeftJoystick.getY(),  RightJoystick.getY());
+   m_DriveTrainSubsystem.Drive( LeftJoystick.getRawAxis(2),  RightJoystick.getRawAxis(2));
    //change the number after the * to adjust the output or whatever 
   }
 
