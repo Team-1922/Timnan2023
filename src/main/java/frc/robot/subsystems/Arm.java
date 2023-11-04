9c16e1262899fdb33cec63ed3a6f4d46f7fef6e3
Teddy E <122415127+TeddyE-E@users.noreply.github.com>
2023-11-04 07:32:04 -0400

    Revert "Trying to fix Tinman17"
    This reverts commit f006591e9f05635d4f6133139be3642c02d630aa.

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants;
 
-import com.playingwithfusion.TimeOfFlight;
+//import com.playingwithfusion.TimeOfFlight;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.SparkMaxAbsoluteEncoder.Type;
